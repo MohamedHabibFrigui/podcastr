c6fb0b5850ebe14fb07e310902c980fb9b7f5e85
Mohamed Habib Frigui <mohamedhabibfrigui9@gmail.com>
2024-06-02 11:30:36 +0100

    File & Folder Structure

---
+++ app/(root)/podcast/[podcastId]/page.tsx
@@ -0,0 +1,5 @@
+const PodcastDetailsPage = ({ params }: { params: { podcastId: string } }) => {
+  return <p className="text-white-1">Podcast Details for {params.podcastId}</p>;
+};
+
+export default PodcastDetailsPage;
