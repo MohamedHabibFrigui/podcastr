2b0b8a89a616e4b0091ca05756ca13ff211b749c
Mohamed Habib Frigui <mohamedhabibfrigui9@gmail.com>
2024-06-04 19:13:32 +0100

    Add generation image functionality with openai

--- components/GenerateThumbnail.tsx
+++ components/GenerateThumbnail.tsx
@@ -9,8 +9,9 @@ import { Input } from "./ui/input";
 import Image from "next/image";
 import { useToast } from "./ui/use-toast";
 import { useUploadFiles } from "@xixixao/uploadstuff/react";
-import { useMutation } from "convex/react";
+import { useAction, useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
+import { v4 as uuidv4 } from "uuid";
 
 const GenerateThumbnail = ({
   setImage,
@@ -30,6 +31,8 @@ const GenerateThumbnail = ({
   const { startUpload } = useUploadFiles(generageUploadUrl);
 
   const getImageUrl = useMutation(api.podcasts.getUrl);
+
+  const handleGenerateThumbnail = useAction(api.openai.generateThumbnailAction);
 
   const handleImage = async (blob: Blob, fileName: string) => {
     setIsImageLoading(true);
@@ -59,7 +62,21 @@ const GenerateThumbnail = ({
     }
   };
 
-  const generateImage = async () => {};
+  const generateImage = async () => {
+    try {
+      const response = await handleGenerateThumbnail({
+        prompt: imagePrompt,
+      });
+      const blob = new Blob([response], { type: "image/png" });
+      handleImage(blob, `thumbnail-${uuidv4()}.png`);
+    } catch (error) {
+      console.log(error);
+      toast({
+        title: "Error generating thumbnail",
+        variant: "destructive",
+      });
+    }
+  };
 
   const uploadImage = async (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
