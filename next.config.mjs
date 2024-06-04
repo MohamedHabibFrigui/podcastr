b1fcd6d8d89be0b4c8b8ee02e45d5a65a90b7407
Mohamed Habib Frigui <mohamedhabibfrigui9@gmail.com>
2024-06-04 18:10:44 +0100

    Add GenerateThumbnail with file upload

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "lovely-flamingo-139.convex.cloud",
       },
+      {
+        protocol: "https",
+        hostname: "colorful-squirrel-282.convex.cloud",
+      },
     ],
   },
 };
