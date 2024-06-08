ac5b8286e58eb67909daf3388c3da16813508ac1
Mohamed Habib Frigui <mohamedhabibfrigui9@gmail.com>
2024-06-08 18:53:02 +0100

    Add profile page

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "colorful-squirrel-282.convex.cloud",
       },
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
     ],
   },
 };
